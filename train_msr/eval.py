0e8d3512d3bf4a44ce5edcb6a9935c2b358fc15e
xiaoaaa2 <138477230+xiaoaaa2@users.noreply.github.com>
2024-02-20 19:05:59 +0800

    Update eval.py

--- train_msr/eval.py
+++ train_msr/eval.py
@@ -7,19 +7,19 @@ from macls.utils.utils import add_arguments, print_arguments
 
 parser = argparse.ArgumentParser(description=__doc__)
 add_arg = functools.partial(add_arguments, argparser=parser)
-add_arg('configs',          str,   'configs/cam++.yml',         "配置文件")
-add_arg("use_gpu",          bool,  True,                        "是否使用GPU评估模型")
-add_arg('save_matrix_path', str,   'output/images/',            "保存混合矩阵的路径")
-add_arg('resume_model',     str,   'models/CAMPPlus_Fbank/best_model/',  "模型的路径")
+add_arg('configs',          str,   'configs/cam++.yml',         "Configuration file")
+add_arg("use_gpu",          bool,  True,                        "Whether to use GPU to evaluate the model")
+add_arg('save_matrix_path', str,   'output/images/',            "The path to save the mixing matrix")
+add_arg('resume_model',     str,   'models/CAMPPlus_Fbank/best_model/',  "The path of the model")
 args = parser.parse_args()
 print_arguments(args=args)
 
-# 获取训练器
+# Retrieve the trainer
 trainer = MAClsTrainer(configs=args.configs, use_gpu=args.use_gpu)
 
-# 开始评估
+# Evaluate the model
 start = time.time()
 loss, accuracy = trainer.evaluate(resume_model=args.resume_model,
                                   save_matrix_path=args.save_matrix_path)
 end = time.time()
-print('评估消耗时间：{}s，loss：{:.5f}，accuracy：{:.5f}'.format(int(end - start), loss, accuracy))
+print('Evaluation time consumption：{}s，loss：{:.5f}，accuracy：{:.5f}'.format(int(end - start), loss, accuracy))
