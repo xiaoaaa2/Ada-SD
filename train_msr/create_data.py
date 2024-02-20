5c9dbdedb365161d96dcfa26599ba36ced58a245
xiaoaaa2 <138477230+xiaoaaa2@users.noreply.github.com>
2024-02-20 16:20:22 +0800

    Create create_data.py

---
+++ train_msr/create_data.py
@@ -0,0 +1,29 @@
+import os
+
+
+# 生成数据列表
+def get_data_list(audio_path, list_path):
+    sound_sum = 0
+    audios = os.listdir(audio_path)
+
+    f_train = open(os.path.join(list_path, 'train_list.txt'), 'w', encoding='utf-8')
+    f_test = open(os.path.join(list_path, 'test_list.txt'), 'w', encoding='utf-8')
+    f_label = open(os.path.join(list_path, 'label_list.txt'), 'w', encoding='utf-8')
+
+    for i in range(len(audios)):
+        f_label.write(f'{audios[i]}\n')
+        sounds = os.listdir(os.path.join(audio_path, audios[i]))
+        for sound in sounds:
+            sound_path = os.path.join(audio_path, audios[i], sound).replace('\\', '/')
+            if sound_sum % 10 == 0:
+                f_test.write('%s\t%d\n' % (sound_path, i))
+            else:
+                f_train.write('%s\t%d\n' % (sound_path, i))
+            sound_sum += 1
+        print("Audio：%d/%d" % (i + 1, len(audios)))
+    f_label.close()
+    f_test.close()
+
+if __name__ == '__main__':
+    get_data_list(audio_path='/home/dataset/audio', 
+                  list_path= '/home/dataset')
